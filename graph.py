b05800ba1f83002ae09a48b035decd017f7a4566
Muqeeth <muqeeth101@gmail.com>
2018-07-05 21:27:19 +0530

    graphfor2hops

--- graph.py
+++ graph.py
@@ -1,12 +1,31 @@
 from graphviz import Digraph
 from graphviz import Graph
+from sc1 import ILP
 
 dot = Digraph(comment='Mesh network',format='png')
-n = 5
+
+n = None
+try:
+	n = raw_input("enter n value(assume max of 2 hops): ")
+except:
+	n = 5
+
+n = int(n)
+d,C = ILP(n)
+
+
 for i in range(2,n+1):
     for j in range(1,n+1):
-        dot.node(str(j))
         if(i!=j):
-            dot.edge(str(i),str(j))
+            if(d["x"+str(i)+str(j)]==1.0):
+                dot.attr('edge',color='green',label = str(C["x"+str(i)+str(j)]))
+                # if(j!=1):
+                #     dot.attr('edge',constraint='false')
+                dot.edge(str(i),str(j))
+            else:
+                dot.attr('edge',color='red',label = str(C["x"+str(i)+str(j)]))
+                # if(j!=1):
+                #     dot.attr('edge',constraint='false')
+                dot.edge(str(i),str(j))
 
 dot.render('mesh.gv', view=True)
