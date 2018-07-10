dfc6546ef96ea21b285335cd0da29a16bd4911e0
Muqeeth <muqeeth101@gmail.com>
2018-07-10 15:03:28 +0530

    comments in graph

--- graph2.py
+++ graph2.py
@@ -1,62 +1,22 @@
+#import graphviz 
 from graphviz import Digraph
 from graphviz import Graph
+#import lpsolver function in multilayer.py
 from multilayer import multilayer_solver
+#below are imports for layered graph
+# import networkx as nx
+# import pygraphviz as pgv # pygraphviz should be available
 
 dot = Digraph(comment='Mesh network',format='png')
 dot2 = Digraph(comment='Mesh network',format='png')
 
-# n = None
-# m = None
-# try:
-# 	m = raw_input("enter m value(receiving nodes): ")
-# except:
-# 	m = 5
-
-# m = int(m)
-
-# try:
-# 	n = raw_input("enter n value(n-m transmitting nodes): ")
-# except:
-# 	n = 5
-
-# n = int(n)
-
-# status,d,C,E = Lpsolver(m,n,[10]*m)
-
-# print status
-# print C
-# print d
-k = 2
-l = [1,8]
+k = 3 #number of layers
+l = [1,10,10] # number of nodes in each layer
 X,Weights,E = multilayer_solver(k,l)
-# print X,"\n"
-# print Weights
 n = sum(l)
 
 
-# for i in range(n-m,n+1):
-#     for j in range(1,n+1):
-#         for k in range(0,m+1):
-#             r = d.get("x"+str(i)+str(j)+str(k))
-#             if(r!=None):
-#                 if(i!=j):
-#                     if(k==0):
-#                         if(d["x"+str(i)+str(j)+str(k)]==1.0):
-#                             dot.attr('edge',color='green',label = str(C["x"+str(i)+str(j)+str(k)]))
-#                             dot.edge(str(i),str(j))
-#                         else:
-#                             dot.attr('edge',color='red',label = str(C["x"+str(i)+str(j)+str(k)]))
-#                             dot.edge(str(i),str(j))
-#                     else:
-#                         if(d["x"+str(i)+str(j)+str(k)]==1.0):
-#                             dot.attr('edge',color='green',label = str(C["x"+str(i)+str(j)+str(k)]))
-#                             dot.edge(str(i),str(j))
-#                             dot.edge(str(j),str(k))
-#                         else:
-#                             dot.attr('edge',color='blue',label = str(C["x"+str(i)+str(j)+str(k)]))
-#                             dot.edge(str(i),str(j))
-#                             dot.edge(str(j),str(k))
-
+#to generate input graph with given data
 for i in range(1,n+1):
     for j in range(1,n+1):
             r = Weights.get("x"+'_'+str(i)+'_'+str(j))
@@ -65,6 +25,7 @@ for i in range(1,n+1):
                     dot.attr('edge',color='red',label = str(Weights["x"+'_'+str(i)+'_'+str(j)]))
                     dot.edge(str(i),str(j))
 
+#to generate output based on answers of lpsolver
 for i in X.keys():
     if(X[i]==1.0):
         r = Weights.get("_".join(i.split('_')[:-1]))
@@ -74,3 +35,22 @@ for i in X.keys():
 
 dot.render('mesh2.gv', view=True)
 dot2.render('meshclean.gv',view=True)
+
+#below is to generate layered graph of input
+# G = nx.DiGraph()
+# for i in Weights.keys():
+#     if(i.split('_')[1]!=i.split('_')[2]):
+#         G.add_edge(i.split('_')[1],i.split('_')[2],color='red',label = Weights[i])
+
+
+# A = nx.nx_agraph.to_agraph(G)
+# p = 1
+# for i in range(1,k+1):
+#     a = []
+#     for j in range(p,p+l[i-1]):
+#         a.append(str(j))
+#     A.add_subgraph(a,rank='same')
+#     p = p+l[i-1]
+    
+# A.draw('example.png', prog='dot')
+
